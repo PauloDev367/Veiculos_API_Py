1772d48d53b4a8cf1932cf3ffbfe4606090eeed9
Paulo Silva <pauloecomercial367@gmail.com>
2025-03-01 13:21:47 -0300

    :sparkles: models do projeto

--- veiculos/carros/models.py
+++ veiculos/carros/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        abstract = True
+
+class Category(Base):
+    name = models.CharField(max_length=255,unique=True)
+
+class Vehicle(Base):
+    mileage = models.FloatField()
+    year = models.IntegerField()
+    name = models.CharField(max_length=255)
+    main_photo = models.ImageField()
+    color = models.CharField(max_length=100)
+    fuel_type = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name='categories', on_delete=models.SET_NULL, blank=True, null=True)
+    price = models.FloatField()
+
+class Photo(Base):
+    path = models.ImageField()
+    vehicle = models.ForeignKey(Vehicle, related_name='vehicles_photos', on_delete=models.SET_NULL, blank=True, null=True)
+    
+class VehicleVariation(Base):
+    name = models.CharField(max_length=255)
+    main_photo = models.ImageField()
+    color = models.CharField(max_length=100)
+    vehicle = models.ForeignKey(Vehicle, related_name='vehicles', on_delete=models.SET_NULL, blank=True, null=True)
