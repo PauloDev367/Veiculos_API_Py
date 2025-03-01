7d81c10d88e8c9ee849b96774d4771adf0117def
Paulo Silva <pauloecomercial367@gmail.com>
2025-03-01 17:04:22 -0300

    :sparkles: trocando sistema de banco de dados de sqlite para mysql

--- veiculos/veiculos/settings.py
+++ veiculos/veiculos/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'veiculos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'veiculosapidjango',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',      
     }
 }
 
